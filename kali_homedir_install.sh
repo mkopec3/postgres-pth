5c9daf4e8da79f95d042a1debbddacfeee981543
mkopec3 <marcin.kopec@windowslive.com>
2018-11-05 20:38:32 +0100

    Added Kali quick install script

---
+++ kali_homedir_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CPUS=`nproc`
+INSTALL_DIR=$HOME/psql-pth
+
+cd && git clone https://github.com/mkopec3/postgres-pth.git && \
+cd postgres-pth && ./configure --prefix=$INSTALL_DIR && \
+make -j$CPUS -C src/bin/ install && \
+make -j$CPUS -C src/interfaces/ install && \
+echo "[*] Binaries have been successfully placed in: $INSTALL_DIR/bin"
